895a1df4095aedb9ab5499431356d02713c0122e
Abdulellah Alamer <122591461+AbdulellahAlamer@users.noreply.github.com>
2024-03-03 09:25:05 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -384,7 +384,7 @@ $(document).ready(function () {
           getSeries() = the lines of the groups, whcih each of them is (red OR blue OR ...)
 
           */
-          labels: ["", titles[2], titles[3], titles[0], titles[1]],
+          labels: ["", titles[0], titles[1], titles[2], titles[3]],
           series: getSeries(),
         },
         {
